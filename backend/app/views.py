85b03f6d25631f998d18764425de6c5c482d4520
Henrique Teixeira <Henrique T>
2024-05-16 23:38:24 +0100

    firt htmx aproach

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 main_blueprint = Blueprint('main', __name__, template_folder='../templates')
 
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+@main_blueprint.route('/data')
+def get_data():
+    data = {
+        'message': 'Dados carregados com sucesso!',
+        'items': ['Item 1', 'Item 2', 'Item 3']
+    }
+    return jsonify(data)
